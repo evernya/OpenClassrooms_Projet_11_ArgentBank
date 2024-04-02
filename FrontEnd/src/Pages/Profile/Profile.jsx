cbf2fae03810264a093a0c26108805673b93413a
EmmaCYC <emmanuelle.cyc@live.fr>
2024-04-02 18:17:48 +0200

    redirection si utilisateur déconnecter

--- FrontEnd/src/Pages/Profile/Profile.jsx
+++ FrontEnd/src/Pages/Profile/Profile.jsx
@@ -1,5 +1,11 @@
+//IMPORT
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 //COMPONENTS
 import EditName from '../../Components/EditName/EditName';
+
 //ASSET
 import accountData from '../../accountData.json';
 
@@ -9,26 +15,38 @@ import '../../Style/style.css'
 
 
 function Profile() {
-    //affichage (render)
-    return (
-      <main className="main bg-dark">
-        <EditName />
-        <h2 className="sr-only">Accounts</h2>
-        {accountData.accounts.map((account, index) => (
-          <section key={index} className="account">
-            <div className="account__content">
-              <h3 className="account__content--title">{account.title}</h3>
-              <p className="account__content--amount">{account.amount}</p>
-              <p className="account__content--amount__description">{account.description}</p>
-            </div>
-  
-            <div className="account__content--cta">
-              <button className="transaction-button">View transactions</button>
-            </div>
-          </section>
-        ))}
-      </main>
-    );
-  }
-  
-  export default Profile;
+  //state
+  const navigate = useNavigate();
+  const authToken = useSelector(state => state.auth.token);
+
+  useEffect(() => {
+    // Si l'utilisateur déconnecté redirection page d'accueil
+    if (!authToken) {
+      navigate("/");
+    }
+  }, [authToken, navigate]);
+
+
+  //affichage (render)
+  return (
+    <main className="main bg-dark">
+      <EditName />
+      <h2 className="sr-only">Accounts</h2>
+      {accountData.accounts.map((account, index) => (
+        <section key={index} className="account">
+          <div className="account__content">
+            <h3 className="account__content--title">{account.title}</h3>
+            <p className="account__content--amount">{account.amount}</p>
+            <p className="account__content--amount__description">{account.description}</p>
+          </div>
+
+          <div className="account__content--cta">
+            <button className="transaction-button">View transactions</button>
+          </div>
+        </section>
+      ))}
+    </main>
+  );
+}
+
+export default Profile;
